2a79c892de4fc8f9c9fb631ae522c40b9181b6fc
BobLiu <bobliu@bobliu.tech>
2025-12-28 21:43:55 +0800

    feat: init project

---
+++ src/main/kotlin/tech/bobliu/assignment08/Assignment08Application.kt
@@ -0,0 +1,11 @@
+package tech.bobliu.assignment08
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Assignment08Application
+
+fun main(args: Array<String>) {
+    runApplication<Assignment08Application>(*args)
+}
